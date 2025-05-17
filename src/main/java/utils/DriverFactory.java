e5078c6266539978617d9b41a015ca0241bf6ff9
111-Anand <vikramadityaa1122@gmail.com>
2025-05-17 13:23:56 +0530

    Publish Allure report

--- src/main/java/utils/DriverFactory.java
+++ src/main/java/utils/DriverFactory.java
@@ -2,12 +2,19 @@ package utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
     private static final ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
     public static WebDriver initDriver() {
-        WebDriver webDriver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new"); // Suitable headless mode for CI
+        options.addArguments("--no-sandbox"); // Bypass OS security model (required in CI)
+        options.addArguments("--disable-dev-shm-usage"); // Overcome limited resource issues
+        options.addArguments("--user-data-dir=/tmp/chrome-user-data"); // Unique temp dir
+
+        WebDriver webDriver = new ChromeDriver(options);
         driver.set(webDriver);
         getDriver().manage().window().maximize();
         return getDriver();
